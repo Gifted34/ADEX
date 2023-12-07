e28f416b97e9bda4ca1ddc03b07b53bb5f2f2a84
Yambanso <yambanso22@gmail.com>
2023-12-07 09:38:53 +0200

    update intialisation

--- src/components/widgets/update.dataStore.dexEntry.jsx
+++ src/components/widgets/update.dataStore.dexEntry.jsx
@@ -1,4 +1,5 @@
 import {
+  AlertBar,
   Box,
   Button,
   ButtonStrip,
@@ -20,18 +21,60 @@ export default function UpdateDataInitialization(props) {
   });
   const [selected,setSelected] = useState()
   const [request,setRequest] = useState()
+
+  const [orgUnitIdScheme,setorgUnitIdScheme] = useState()
+  const [dataElementIdScheme,setdataElementIdScheme] = useState()
+
+  const [orgAttributes,setOrgAttributes] = useState()
+  const [dxAttributes,setdxAttributes] = useState()
+
+  const [message,setMessage] = useState()
+  const [open,setOpen] = useState(false)
   const update = (e) => {
-    //props?.updateGeneralInputValues({ data: props?.data, values: data });
-    console.log(request)
-    console.log({...data,request: request})
+    setData({...data,request: request})
+    if(orgUnitIdScheme === 'attribute' && orgAttributes === undefined){
+      setMessage('Please select the organisation input attribute')
+      setOpen(true)
+    }else if(dataElementIdScheme === 'attribute' && dxAttributes === undefined){
+      setMessage('Please select the data input attribute')
+      setOpen(true)
+    }else{
+    props?.updateGeneralInputValues({ data: props?.data, values: {...data,request: request} });
+    }
   };
 
+  const initialisingState = () => {
+    if(request?.dataElementIdScheme !== 'UID' || request?.dataElementIdScheme !== 'code'){
+      const strings = request?.dataElementIdScheme?.split(':')
+      if(strings?.length > 0){
+      setdataElementIdScheme(strings[0])
+      setdxAttributes(strings[1])
+      } 
+    }else{
+      setdataElementIdScheme(request?.dataElementIdScheme)
+    }
+    if(request?.orgUnitIdScheme !== 'UID' || request?.orgUnitIdScheme !== 'code'){
+      const strings = request?.orgUnitIdScheme?.split(':')
+      if(strings?.length > 0){
+      setorgUnitIdScheme (strings[0])
+      setOrgAttributes (strings[1])
+      } 
+    }else{
+      setorgUnitIdScheme (request?.orgUnitIdScheme)
+    }
+  }
+
   useEffect(() => {
-    setSelected(props?.data)
-    setData(props?.data.value);
-    setSelected(data?.request?.idScheme)
-    setRequest(data?.request)
+    setRequest(props?.data?.value?.request)
+    setData(props?.data?.value)
+    setSelected(props?.data?.value?.request?.idScheme)
+    initialisingState()
   }, [props]);
+
+  useEffect(()=>{
+    initialisingState()
+    console.log(request)
+  },[request])
 
   return (
     <div
@@ -67,9 +110,8 @@ export default function UpdateDataInitialization(props) {
                       className="select"
                       placeholder="Select request id scheme"
                       onChange={(e) => {
-                        console.log(e)
                         setSelected(e.selected)
-                        setRequest({ ...request,idScheme: e.selected});
+                        request.idScheme = e.selected
                       }}
                       selected={selected}
                     >
@@ -101,6 +143,67 @@ export default function UpdateDataInitialization(props) {
                         // placeholder={props?.data?.value?.url}
                       />
                     )}
+
+          <Box className={props?.styles?.marginBottom}>
+            <SingleSelect
+              className="select"
+              name='dataElementIdScheme'
+              placeholder="Select request dataElementIdScheme"
+              onChange={(e) => {
+                setdataElementIdScheme(e.selected)
+                if(e.selected !== 'attribute'){
+                  request.dataElementIdScheme = e.selected
+                }
+              }}
+              selected={dataElementIdScheme}
+            >
+              <SingleSelectOption label="UID" value="UID" />
+              <SingleSelectOption label="Code" value="code" />
+              <SingleSelectOption label="Attribute" value="attribute" />
+            </SingleSelect>
+          </Box>
+          {dataElementIdScheme === 'attribute' &&
+          <Box className={props?.styles?.marginBottom}>
+            <SingleSelect
+            className="select"
+            placeholder="select request dataElementIdScheme attribute"
+            onChange={(e)=>{
+              setdxAttributes(e.selected)
+              request.dataElementIdScheme = `attribute:${e.selected}`
+              }}
+              selected={dxAttributes}>
+              {props?.attributes?.filter(att => att?.objectTypes.includes('DATA_ELEMENT') ||att?.objectTypes.includes('INDICATOR'))
+              .map(att=> <SingleSelectOption label={att.displayName} value={att.id} />)}</SingleSelect></Box>}
+          <Box className={props?.styles?.marginBottom}>
+            <SingleSelect
+              className="select"
+              placeholder="Select request orgUnitIdScheme"
+              onChange={(e) => {
+                setorgUnitIdScheme(e.selected)
+                if(e.selected !== 'attribute'){
+                  request.orgUnitIdScheme =  e.selected
+                }
+              }}
+              selected={orgUnitIdScheme}
+            >
+              <SingleSelectOption label="UID" value="UID" />
+              <SingleSelectOption label="Code" value="code" />
+              <SingleSelectOption label="Attribute" value="attribute" />
+            </SingleSelect>
+          </Box>
+          {orgUnitIdScheme === 'attribute' &&
+          <Box className={props?.styles?.marginBottom}>
+            <SingleSelect
+            className="select"
+            placeholder="select request orgUnitIdScheme attribute"
+            onChange={(e)=>{
+              setOrgAttributes(e.selected)
+              request.orgUnitIdScheme = `attribute:${e.selected}`
+            }
+            }
+              selected={orgAttributes}>
+              {props?.attributes?.filter(att => att?.objectTypes.includes('ORGANISATION_UNIT'))
+              .map(att=> <SingleSelectOption label={att.displayName} value={att.id} />)}</SingleSelect></Box>}
                   </div>
                 </Field>
               </div>
@@ -123,6 +226,15 @@ export default function UpdateDataInitialization(props) {
           </Modal>
         </div>
       )}
+      {open &&
+      <div className={props?.styles?.alertBtm}>
+        <AlertBar
+          warning
+          duration={2000}
+          onHidden={(e)=> setOpen(false)}>
+            {message}
+          </AlertBar>
+        </div>}
     </div>
   );
 }
