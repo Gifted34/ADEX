dcca88576751dba8bf896671d08de42615cd4a31
Yambanso <yambanso22@gmail.com>
2024-03-24 19:45:18 +0200

    navigate to DHIS2 instance

--- src/components/widgets/homePage.jsx
+++ src/components/widgets/homePage.jsx
@@ -2,8 +2,11 @@ import { Button, ButtonStrip, Center, Divider, Layer, NoticeBox } from "@dhis2/u
 import React, { useEffect, useState } from "react";
 import DataInitialized from "./dataInitialized";
 import { useDataEngine } from "@dhis2/app-runtime";
+import { useConfig } from "@dhis2/app-runtime";
+
 
 export default function HomePage(props) {
+  const {baseUrl} = useConfig()
   const engine = useDataEngine();
   let dataStorePath = "dataStore/DEX_initializer_values";
 
@@ -29,10 +32,18 @@ export default function HomePage(props) {
         width: "100%",
       }}
     >
-      {props?.notAuth && <Layer transluscent>
+      {props?.notAuth && <Layer translucent >
         <Center>
         <NoticeBox title="Unauthorised Access" warning >
-                You do not have metadata access(Create) for Aggregate data exchange: Please contact your admin 
+                <div>
+                <p>You do not have metadata access(Create) for Aggregate data exchange: Please contact your admin</p>
+                <a href={baseUrl}>
+                <Button primary                 
+                >
+                  Quit
+                </Button>
+                </a>
+                </div>
               </NoticeBox>
           </Center></Layer>}
       <ButtonStrip end>
